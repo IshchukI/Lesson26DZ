7a6c75cadbc2415f8a38fb7f417a39cc996e2b94
I_Ishchuk <yaroslavischuck@gmail.com>
2022-08-22 23:39:09 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import io
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with io.open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+with io.open("Readme.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+def main():
+    setup(
+        name='jira-ithillel-pageobject',
+        version='1.0',
+        description='My Package Description',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url='https://github.com/IshchukI/Lesson26DZ',
+        author='Iaroslav Ishchuk',
+        author_email='contact@demo.com',
+        packages=['src',
+                  'src.pages'],
+        install_requires=required,
+        include_package_data=True,
+        keywords="mypackage for product test",
+
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+from .pages import header_page
+from .pages import login_page
+
+class Header_page(header_page.HeaderPage):
+    pass
+
+class Login_page(login_page.LoginPage):
+    pass
